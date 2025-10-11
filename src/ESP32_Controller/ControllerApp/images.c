94459f113338da5eac253e75319fb66a9660a037
P!X <haslinger-mario@gmx.at>
2025-10-12 01:13:42 +0200

    CLEAR

--- src/ESP32_Controller/ControllerApp/images.c
+++
@@ -1,8 +0,0 @@
-#include "images.h"
-
-const ext_img_desc_t images[4] = {
-    { "ICON_FAN", &img_icon_fan },
-    { "ICON_MENU_RELEASED", &img_icon_menu_released },
-    { "ICON_MENU_PRESSED", &img_icon_menu_pressed },
-    { "ICON_DOOR_OPENED", &img_icon_door_opened },
-};
